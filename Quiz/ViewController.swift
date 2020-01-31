ea05eb4068433f9afb99983d6f9bb38ff5550266
Sathvik Chandupatla <schan9@unh.newhaven.edu>
2020-01-30 20:14:08 -0500

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Chandupatla, Sai Sathvik Kumar on 1/30/20.
+//  Copyright © 2020 Chandupatla, Sai Sathvik Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
