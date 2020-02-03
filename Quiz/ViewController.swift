83274839cb2ca64a3a06003b97196c02a5f0d415
Sathvik Chandupatla <schan9@unh.newhaven.edu>
2020-02-02 19:37:57 -0500

    commit test
    commit test

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
     super.viewDidLoad()
-        os_log("I just Loaded")
+        os_log("I just Loaded") 
   
 }
 }
